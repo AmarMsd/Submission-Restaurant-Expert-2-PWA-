398272e8aafa48f59bb63a36317340ba1fc7ccbb
Marmar <71787768+AmarMsd@users.noreply.github.com>
2022-06-19 00:37:17 +0700

    Add files via upload

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const ServiceWorkerWebpackPlugin = require("serviceworker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/scripts/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/templates/index.html"),
+      filename: "index.html",
+    }),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.resolve(__dirname, "src/scripts/sw.js"),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/public/"),
+          to: path.resolve(__dirname, "dist/"),
+        },
+      ],
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+    }),
+  ],
+};
